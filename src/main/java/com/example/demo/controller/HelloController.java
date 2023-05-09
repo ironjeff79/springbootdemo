7b9c8b0d4e88b0aec4e1556698f9912df0217516
ironjeff79 <ironjeffery79@gmail.com>
2023-05-09 19:49:50 +0900

    demo

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.example.demo.controller;public class HelloController {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+// http://localhost:8080/hello
+// http://localhost:8080/hello?nickname=zhangsan&phone=123
+    @GetMapping("/hello")
+    public String hello(String nickname,String phone){
+        System.out.println(phone);
+        return "hello" + nickname;
+    }
 }
